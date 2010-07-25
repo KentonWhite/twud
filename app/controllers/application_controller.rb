7f57f4d6ca4cfa63024475854c85c5cd6eedd3e0
KentonWhite <jkentonwhite@gmail.com>
2010-07-25 13:50:22 -0400

    Intial setup with user authentication

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,6 @@
+class ApplicationController < ActionController::Base
+  include Authentication
+  protect_from_forgery
+  layout 'application' 
+  before_filter :login_required
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '13fc428abb07b5da55aa87394d2e3681e7aebb4ede8b63c9a8eb2a731b8b19a8fa52642fd118e9d0db49271e82c2c7e84e3e690f65561e2a45747c22eaf8a9c8'
