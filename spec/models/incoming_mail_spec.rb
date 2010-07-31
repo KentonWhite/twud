25f58bb9145fc4de0eb9f5c99390181ba09d56f4
KentonWhite <jkentonwhite@gmail.com>
2010-07-31 12:14:01 -0400

    Created incoming_mail_spec with basic check that a new mail is valid

--- spec/models/incoming_mail_spec.rb
+++ spec/models/incoming_mail_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe IncomingMail do
-  pending "add some examples to (or delete) #{__FILE__}"
+  def new_mail(attributes = {})
+    attributes[:to] ||= 'bar@example.com'
+    attributes[:from] ||= 'foo@example.com'
+    attributes[:subject] ||= 'Sample Subject'
+    attributes[:text] ||= 'Sample Text'  
+    attributes[:html] ||= '<p>Sample Text</p>'
+    IncomingMail.new(attributes)
+  end
+  
+  before(:each) do
+    IncomingMail.delete_all
+  end
+  
+  it "should be valid" do
+    new_mail.should be_valid
+  end
 end
+
