066a7b1d7337085e5493d562ecf7349c8b970e46
KentonWhite <jkentonwhite@gmail.com>
2010-07-30 21:01:52 -0400

    quick test of autoregister form tweitter

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,7 +3,7 @@ class PlayersController < ApplicationController
   skip_before_filter :verify_authenticity_token, :login_required 
   
   def register
-    @incoming_mail = IncomingMail.new(
+    incoming_mail = IncomingMail.new(
       :text => params['text'],
       :html => params['html'],
       :to => params['to'],
@@ -11,7 +11,10 @@ class PlayersController < ApplicationController
       :subject => params['subject']
     )                              
     
-    @incoming_mail.save
-    render :xml => "incoming_mail", :status => :created
+    incoming_mail.save
+    render :xml => incoming_mail.to_xml, :status => :created 
+     
+    name = incoming_mail.text.match(/http:\/\/twitter.com\/(.*)/)[1]
+    player = Player.new(:name => name)
   end
 end
